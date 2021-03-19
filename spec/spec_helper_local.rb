dea01c851cdbb8641b71732298e35c996f197a07
Heston Snodgrass <hsnodgrass3@gmail.com>
2021-03-19 12:41:20 -0700

    Added proper stubs / mocks to server_spec.rb

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -3,16 +3,5 @@
 #
 # undefined method `symlink?' for Puppet::Util::Windows::File:Module
 RSpec.configure do |c|
-  c.mock_with :rspec do |mocks|
-    # Necessary to use allow_any_instance_of
-    mocks.syntax = [:expect, :should]
-  end
-
-  c.before :each do
-    # rubocop:disable RSpec/AnyInstance
-    allow_any_instance_of(
-      Puppet::Type.type(:acl).provider(:windows),
-    ).to(receive(:validate))
-    # rubocop:enable RSpec/AnyInstance
-  end
+  c.mock_with :rspec
 end
