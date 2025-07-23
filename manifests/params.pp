494fae69bbc30cfe7e55cad09e6067311be844b9
shubhamperforce <143171466+shubhamperforce@users.noreply.github.com>
2025-07-23 12:05:02 +0530

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class ssh::params (
   Boolean $cygwin = lookup('cygwin::enable', Boolean, undef, false),
 ) {
   case $facts['os']['name'] {
-    'AlmaLinux', 'CentOS', 'RedHat', 'Fedora': {
+    'AlmaLinux', 'Amazon', 'CentOS', 'RedHat', 'Fedora': {
       $server_class      = 'ssh::server::linux'
       $client_package    = 'openssh-clients'
       $server_package    = 'openssh-server'
